2c91ae5ddb112e1396c17f361e0da685137a2cfc
Barbra1 <barbara.gutierrez94@hotmail.com>
2017-04-11 22:59:40 -0300

    barraMenu

---
+++ head/barraMenu.php
@@ -0,0 +1,8 @@
+<div class="topnav">
+      <a class="active" href="#home">Face It</a>
+      <a href="#about">¿Quienes somos?</a>
+      <a href="#contact">Contacto</a>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a id="inicioSesion" href="#login"><span class="glyphicon glyphicon-log-in"></span> Iniciar sesión</a></li>
+      </ul>
+    </div>
