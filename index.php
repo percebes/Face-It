af569d3742f0a11e4aa582b8e1952c4fc06f4ed5
Barbra1 <barbara.gutierrez94@hotmail.com>
2017-04-11 22:11:35 -0300

    barbara update

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Face It</title>
+        <script src="js/phaser.js"></script>
+        <LINK REL=StyleSheet HREF="css/style.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    
+    <body>
+    	
+   <!--<nav>
+        <ul class="menu">
+           <li><a href="#"><i class="icon-home"></i>HOME</a>
+               <ul class="sub-menu">
+                   <li><a href="#">Sub-Menu 1</a></li>
+                   <li><a href="#">Sub-Menu 2</a></li>
+                   <li><a href="#">Sub-Menu 3</a></li>
+               </ul>
+           </li>
+          <li><a  href="#"><i class="icon-user"></i>ABOUT</a></li>
+          <li><a  href="#"><i class="icon-camera"></i>PORTFOLIO</a>
+              <ul class="sub-menu">
+                   <li><a href="#">Sub-Menu 1</a></li>
+                   <li><a href="#">Level 3 Menu</a>
+                        <ul>
+                            <li><a href="#">Sub-Menu 4</a></li>
+                            <li><a href="#">Sub-Menu 5</a></li>
+                            <li><a href="#">Sub-Menu 6</a></li>
+                        </ul>
+                   </li>
+               </ul>
+          </li>
+          <li><a  href="#"><i class="icon-bullhorn"></i>BLOG</a></li>
+          <li><a  href="#"><i class="icon-envelope-alt"></i>CONTACT</a></li>
+      </ul>
+  </nav>-->
+    
+    <div class="topnav">
+      <a class="active" href="#home">Face It</a>
+      <a href="#About">¿Quienes somos?</a>
+      <a href="#contacto">Contacto</a>
+      <a id="inicioSesion" href="#about">Iniciar sesión</a>
+    </div>
+
+
+
+    </body>
+</html>
