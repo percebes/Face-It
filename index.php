be6f3f8ba8b9af0dd5cf178bc85303e32c8d60cd
Barbra1 <barbara.gutierrez94@hotmail.com>
2017-04-11 23:29:48 -0300

    barbara

--- index.php
+++ index.php
@@ -3,23 +3,11 @@
     <head>
         <meta charset="UTF-8" />
         <title>Face It</title>
-        <script src="js/phaser.js"></script>
-        <LINK REL=StyleSheet HREF="css/style.css"/>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <?php
             include("head/barraMenu.php");
-
         ?>
-        
     </head>
-    
     <body>
-    	
-
-    
-
-
     </body>
 </html>
+HOLA ESTO ES FACE IT
